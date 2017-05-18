312687475cdaef4ac3d958af370a3c2e39e2be45
soundari <soundarivenkat77@gmail.com>
2017-05-18 02:05:31 -0700

    Initial commit

---
+++ src/main/java/com/jpr/pubhub100/Pubhub100Application.java
@@ -0,0 +1,12 @@
+package com.jpr.pubhub100;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pubhub100Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pubhub100Application.class, args);
+	}
+}
